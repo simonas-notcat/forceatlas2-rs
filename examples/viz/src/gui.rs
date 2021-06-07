70b2e47ba77950f0d1047e35d50001f13e2b0633
ZettaScript <contact@zettascript.org>
2021-06-07 23:25:49 +0200

    Viz: zoom, fix resize, opti

--- examples/viz/src/gui.rs
+++ examples/viz/src/gui.rs
@@ -20,8 +20,14 @@ const INVALID_BG_COLOR: gdk::RGBA = gdk::RGBA {
 	alpha: 0.1,
 };
 
-enum Msg {
+enum MsgToGtk {
+	Resize,
 	Update,
+}
+
+struct MsgFromGtk {
+	redraw: bool,
+	resize: bool,
 }
 
 struct Pixbuf(gdk_pixbuf::Pixbuf);
@@ -31,13 +37,14 @@ unsafe impl Sync for Pixbuf {}
 
 fn build_ui(
 	app: &gtk::Application,
-	rx: Arc<RwLock<Option<glib::Receiver<Msg>>>>,
+	rx: Arc<RwLock<Option<glib::Receiver<MsgToGtk>>>>,
+	tx: Arc<RwLock<MsgFromGtk>>,
 	compute: Arc<RwLock<bool>>,
 	layout: Arc<RwLock<Layout<T>>>,
 	settings: Arc<RwLock<Settings<T>>>,
 	pixbuf: Arc<RwLock<Option<Pixbuf>>>,
-	redraw: Arc<RwLock<bool>>,
 	edge_color: Arc<RwLock<(u8, u8, u8)>>,
+	zoom: Arc<RwLock<T>>,
 ) {
 	let glade_src = include_str!("gui.glade");
 	let builder = gtk::Builder::new();
@@ -50,11 +57,15 @@ fn build_ui(
 	let graph_box: gtk::ScrolledWindow = builder.get_object("graph_box").unwrap();
 	let compute_button: gtk::ToggleButton = builder.get_object("bt_compute").unwrap();
 	let reset_button: gtk::Button = builder.get_object("bt_reset").unwrap();
+	let save_img_button: gtk::Button = builder.get_object("bt_save_img").unwrap();
 	let chunk_size_input: gtk::Entry = builder.get_object("chunk_size").unwrap();
 	let ka_input: gtk::Entry = builder.get_object("ka").unwrap();
 	let kg_input: gtk::Entry = builder.get_object("kg").unwrap();
 	let kr_input: gtk::Entry = builder.get_object("kr").unwrap();
 	let edge_color_input: gtk::ColorButton = builder.get_object("edge_color").unwrap();
+	let zoom_input: gtk::Entry = builder.get_object("zoom").unwrap();
+
+	let save_img_window: gtk::FileChooserDialog = builder.get_object("save_img_window").unwrap();
 
 	{
 		let settings = settings.read().unwrap();
@@ -63,13 +74,41 @@ fn build_ui(
 		kg_input.set_text(&settings.kg.to_string());
 		kr_input.set_text(&settings.kr.to_string());
 	}
+	zoom_input.set_text(&zoom.read().unwrap().to_string());
+
+	graph_area.connect_key_press_event({
+		let zoom_input = zoom_input.clone();
+		let zoom = zoom.clone();
+		move |_, event| {
+			if let Some(key) = event.get_keyval().name() {
+				match key.as_str() {
+					"KP_Add" | "plus" => {
+						let zoom = *zoom.read().unwrap();
+						zoom_input.set_text(&(zoom * 1.1).to_string());
+					}
+					"KP_Subtract" | "minus" => {
+						let zoom = *zoom.read().unwrap();
+						zoom_input.set_text(&(zoom * (1. / 1.1)).to_string())
+					}
+					"KP_0" | "0" => zoom_input.set_text("1"),
+					_ => {}
+				}
+			}
+			Inhibit(false)
+		}
+	});
 
 	let graph_area = Rc::new(graph_area);
 	let edge_color_input = Rc::new(edge_color_input);
+	let save_img_window = Rc::new(save_img_window);
 
 	compute_button.connect_toggled({
+		let tx = tx.clone();
 		move |bt| {
 			if let Ok(mut compute) = compute.write() {
+				if *compute {
+					tx.write().unwrap().redraw = true;
+				}
 				*compute = bt.get_active();
 			}
 		}
@@ -77,15 +116,25 @@ fn build_ui(
 
 	reset_button.connect_clicked({
 		let layout = layout.clone();
-		let redraw = redraw.clone();
+		let tx = tx.clone();
 		move |_| {
 			let mut rng = rand::thread_rng();
 			let mut layout = layout.write().unwrap();
 			layout.old_speeds.points.fill(0.0);
 			layout.points.points.fill_with(|| rng.gen_range(-1.0..1.0));
-			*redraw.write().unwrap() = true;
+			tx.write().unwrap().redraw = true;
 		}
 	});
+
+	save_img_button.connect_clicked({
+		let save_img_window = save_img_window.clone();
+		move |_| {
+			save_img_window.show_all();
+		}
+	});
+
+	save_img_window
+		.connect_delete_event(move |save_img_window, _| save_img_window.hide_on_delete());
 
 	chunk_size_input.connect_changed({
 		let layout = layout.clone();
@@ -161,48 +210,84 @@ fn build_ui(
 		}
 	});
 
-	edge_color_input.clone().connect_color_set({
-		let redraw = redraw.clone();
-		move |_| {
+	edge_color_input.connect_color_set({
+		let tx = tx.clone();
+		move |edge_color_input| {
 			let c = edge_color_input.get_rgba();
-			*edge_color.write().unwrap() = ((c.red*255.) as u8, (c.green*255.) as u8, (c.blue*255.) as u8);
-			*redraw.write().unwrap() = true;
+			*edge_color.write().unwrap() = (
+				(c.red * 255.) as u8,
+				(c.green * 255.) as u8,
+				(c.blue * 255.) as u8,
+			);
+			tx.write().unwrap().redraw = true;
+		}
+	});
+
+	zoom_input.connect_changed({
+		let pixbuf = pixbuf.clone();
+		let graph_box = graph_box.clone();
+		let tx = tx.clone();
+		let zoom = zoom.clone();
+		move |entry| {
+			if let Ok(val) = entry.get_buffer().get_text().parse() {
+				if val > 0.0 {
+					entry.override_background_color(gtk::StateFlags::NORMAL, None);
+					let mut zoom = zoom.write().unwrap();
+					let mut pixbuf = pixbuf.write().unwrap();
+					*zoom = val;
+					*pixbuf = gdk_pixbuf::Pixbuf::new(
+						gdk_pixbuf::Colorspace::Rgb,
+						false,
+						8,
+						(graph_box.get_allocated_width() as T * *zoom) as i32,
+						(graph_box.get_allocated_height() as T * *zoom) as i32,
+					)
+					.map(Pixbuf);
+					tx.write().unwrap().redraw = true;
+					return;
+				}
+			}
+			entry.override_background_color(gtk::StateFlags::NORMAL, Some(&INVALID_BG_COLOR));
 		}
 	});
 
 	let resize_handler = {
 		let pixbuf = pixbuf.clone();
+		let tx = tx.clone();
 		move || {
 			let mut pixbuf = pixbuf.write().unwrap();
-			*redraw.write().unwrap() = true;
+			tx.write().unwrap().redraw = true;
+			let zoom = zoom.read().unwrap();
 			*pixbuf = gdk_pixbuf::Pixbuf::new(
 				gdk_pixbuf::Colorspace::Rgb,
 				false,
 				8,
-				graph_box.get_allocated_width(),
-				graph_box.get_allocated_height(),
+				(graph_box.get_allocated_width() as T * *zoom) as i32,
+				(graph_box.get_allocated_height() as T * *zoom) as i32,
 			)
 			.map(Pixbuf);
 		}
 	};
 
 	window.connect_configure_event({
-		let resize_handler = resize_handler.clone();
 		move |_, _| {
-			resize_handler();
+			tx.write().unwrap().resize = true;
 			true
 		}
 	});
 
-	if let Ok(mut rx) = rx.write() {
-		if let Some(rx) = rx.take() {
-			rx.attach(None, move |_| {
-				if let Some(pixbuf) = pixbuf.read().unwrap().as_ref() {
-					graph_area.set_from_pixbuf(Some(&pixbuf.0));
+	if let Some(rx) = rx.write().unwrap().take() {
+		rx.attach(None, move |msg| {
+			match msg {
+				MsgToGtk::Update => {
+					if let Some(pixbuf) = pixbuf.read().unwrap().as_ref() {
+						graph_area.set_from_pixbuf(Some(&pixbuf.0));
+					}
 				}
-				glib::Continue(true)
-			});
-		}
+				MsgToGtk::Resize => resize_handler(),
+			}
+			glib::Continue(true)
+		});
 	}
 
 	window.show_all();
@@ -219,28 +304,33 @@ pub fn run(
 	)
 	.unwrap();
 
-	let (tx, rx) = glib::MainContext::sync_channel(glib::PRIORITY_DEFAULT, 1);
+	let (tx, rx) = glib::MainContext::sync_channel(glib::PRIORITY_DEFAULT, 4);
 	let rx = Arc::new(RwLock::new(Some(rx)));
+	let msg_from_gtk = Arc::new(RwLock::new(MsgFromGtk {
+		redraw: true,
+		resize: false,
+	}));
 	let pixbuf = Arc::new(RwLock::new(None));
-	let redraw = Arc::new(RwLock::new(true));
 	let edge_color = Arc::new(RwLock::new((5, 5, 5)));
+	let zoom = Arc::new(RwLock::new(1.0));
 
 	application.connect_activate({
 		let compute = compute.clone();
 		let layout = layout.clone();
 		let pixbuf = pixbuf.clone();
-		let redraw = redraw.clone();
 		let edge_color = edge_color.clone();
+		let msg_from_gtk = msg_from_gtk.clone();
 		move |app| {
 			build_ui(
 				app,
 				rx.clone(),
+				msg_from_gtk.clone(),
 				compute.clone(),
 				layout.clone(),
 				settings.clone(),
 				pixbuf.clone(),
-				redraw.clone(),
 				edge_color.clone(),
+				zoom.clone(),
 			)
 		}
 	});
@@ -256,13 +346,23 @@ pub fn run(
 					pixbuf.0.get_rowstride(),
 					*edge_color.read().unwrap(),
 				);
-				tx.send(Msg::Update).unwrap();
+				tx.send(MsgToGtk::Update).unwrap();
+			}
+			let mut msg_from_gtk = msg_from_gtk.write().unwrap();
+			if msg_from_gtk.resize {
+				msg_from_gtk.resize = false;
+				msg_from_gtk.redraw = false;
+				tx.send(MsgToGtk::Resize).unwrap();
 			}
 			DRAW_SLEEP
 		} else {
-			let mut redraw = redraw.write().unwrap();
-			if *redraw {
-				*redraw = false;
+			let mut msg_from_gtk = msg_from_gtk.write().unwrap();
+			if msg_from_gtk.resize {
+				msg_from_gtk.resize = false;
+				msg_from_gtk.redraw = false;
+				tx.send(MsgToGtk::Resize).unwrap();
+			} else if msg_from_gtk.redraw {
+				msg_from_gtk.redraw = false;
 				if let Some(pixbuf) = pixbuf.write().unwrap().as_ref() {
 					let layout = layout.read().unwrap();
 					crate::drawer::draw_graph(
@@ -272,7 +372,7 @@ pub fn run(
 						pixbuf.0.get_rowstride(),
 						*edge_color.read().unwrap(),
 					);
-					tx.send(Msg::Update).unwrap();
+					tx.send(MsgToGtk::Update).unwrap();
 				}
 			}
 			STANDBY_SLEEP
